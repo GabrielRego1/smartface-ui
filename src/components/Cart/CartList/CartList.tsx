9f87c6764ee11c6fb47ec9236088f0a280c85f05
GabrielRego1 <gabrielantonio280@gmail.com>
2024-09-28 18:32:02 -0300

    feat: improv css table

--- src/components/Cart/CartList/CartList.tsx
+++ src/components/Cart/CartList/CartList.tsx
@@ -23,55 +23,71 @@ const CartList: React.FC = () => {
   const [openModal, setOpenModal] = useState(false);
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
-  
+
   const handleOpen = () => setOpenModal(true);
   const handleClose = () => setOpenModal(false);
-  
+
   const handleAddCart = (codigo: string) => {
     const newCart: Cart = { id: carts.length + 1, codigo, ativo: true };
     setCarts([...carts, newCart]);
     setSuccess(true);
   };
-  
+
   const handleToggleActive = (id: number) => {
     setCarts(carts.map(cart => cart.id === id ? { ...cart, ativo: !cart.ativo } : cart));
   };
-  
+
   const handleDelete = (id: number) => {
     setCarts(carts.filter(cart => cart.id !== id));
   };
-  
+
   const handleCloseSnackbar = () => {
     setError(false);
     setSuccess(false);
   };
-  
+
   return (
-    <Box sx={{ padding: 2 }}>
+    <Box sx={{ padding: 2, boxShadow:'0 8px 16px rgba(0, 0, 0, 0.5);'}}>
 
       <Box className="box" sx={{ display: 'flex', justifyContent: 'flex-start', mt: 2 }}>
         <Typography variant="h4" component="h1" sx={{ mb: 2, color: 'var(--cor-botao)' }}>Lista de Carrinhos</Typography>
       </Box>
-      <TableContainer component={Paper} sx={{ mt: 2, backgroundColor: 'var(--cor-fundo)' }}>
+      <TableContainer component={Paper} sx={{ mt: 2, backgroundColor: 'var(--cor-fundo)' }} className='table-container'>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell sx={{color:'var(--cor-texto)'}}>ID</TableCell>
-              <TableCell sx={{color:'var(--cor-texto)'}}>Código</TableCell>
-              <TableCell sx={{color:'var(--cor-texto)'}}>Ativo</TableCell>
-              <TableCell sx={{color:'var(--cor-texto)'}}>#</TableCell>
+              <TableCell sx={{ color: 'var(--cor-texto)' }}>ID</TableCell>
+              <TableCell sx={{ color: 'var(--cor-texto)' }}>Código</TableCell>
+              <TableCell sx={{ color: 'var(--cor-texto)' }}>Ativo</TableCell>
+              <TableCell sx={{ color: 'var(--cor-texto)' }}>#</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {carts.map((cart) => (
               <TableRow key={cart.id}>
-                <TableCell sx={{color:'var(--cor-texto)'}}>{cart.id}</TableCell>
-                <TableCell sx={{color:'var(--cor-texto)'}}>{cart.codigo}</TableCell>
-                <TableCell sx={{color:'var(--cor-texto)'}}>
+                <TableCell sx={{ color: 'var(--cor-texto)' }}>{cart.id}</TableCell>
+                <TableCell sx={{ color: 'var(--cor-texto)' }}>{cart.codigo}</TableCell>
+                <TableCell >
                   <Switch
+                    sx={{
+                      ".MuiSwitch-thumb": {
+                        backgroundColor: "var(--cor-botao)"
+                      },
+                      ".MuiSwitch-track": {
+                        backgroundColor: "#e8e8e8"
+                      },
+                      "& .MuiSwitch-switchBase": {
+                        "&.Mui-checked": {
+                          "+ .MuiSwitch-track": {
+                            backgroundColor: "white"
+                          },
+                        }
+                      }
+                    }}
                     checked={cart.ativo}
                     onChange={() => handleToggleActive(cart.id)}
                   />
+
                 </TableCell>
                 <TableCell>
                   <IconButton color="error" onClick={() => handleDelete(cart.id)}>
@@ -90,7 +106,7 @@ const CartList: React.FC = () => {
         onAdd={handleAddCart}
       />
       <Box className="box" sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2 }}>
-        <Button variant="outlined" onClick={handleOpen}>Adicionar Cart</Button>
+        <Button variant="outlined" sx={{ ml: 1, color: 'var(--cor-texto)', borderColor: 'var(--cor-botao)' }} onClick={handleOpen}>Adicionar Cart</Button>
       </Box>
       <Snackbar open={error || success} autoHideDuration={6000} onClose={handleCloseSnackbar}>
         <Alert onClose={handleCloseSnackbar} severity={error ? 'error' : 'success'}>
