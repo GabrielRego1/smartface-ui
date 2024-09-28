201ad9cfbcfceb5e580ce49f61ff27aebf0e41c5
GabrielRego1 <gabrielantonio280@gmail.com>
2024-09-28 19:50:19 -0300

    feat: added status card modal

--- src/components/Cart/CartList/CartList.tsx
+++ src/components/Cart/CartList/CartList.tsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import { Box, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Snackbar, Alert, Switch, IconButton, Typography } from '@mui/material';
 import AddCartModal from '../AddCartModal/AddCartModal';
 import DeleteIcon from '@mui/icons-material/Delete';
+import BatteryChargingFullIcon from '@mui/icons-material/BatteryChargingFull';
 import KeyboardReturn from '@mui/icons-material/KeyboardReturn';
 import { useNavigate } from 'react-router-dom';
+import StatusCartModal from '../StatusCartModal/StatusCartModal';
 
 interface Cart {
   id: number;
@@ -12,22 +14,31 @@ interface Cart {
 }
 
 const initialCarts: Cart[] = [
-  { id: 1, codigo: 'CART001', ativo: true },
-  { id: 2, codigo: 'CART002', ativo: false },
-  { id: 3, codigo: 'CART003', ativo: false },
-  { id: 4, codigo: 'CART004', ativo: true },
-  { id: 5, codigo: 'CART005', ativo: true },
+  { id: 1001, codigo: 'CART001', ativo: true },
+  { id: 1002, codigo: 'CART002', ativo: false },
+  { id: 1003, codigo: 'CART003', ativo: false },
+  { id: 1004, codigo: 'CART004', ativo: true },
+  { id: 1005, codigo: 'CART005', ativo: true },
 ];
 
 const CartList: React.FC = () => {
   const navigate = useNavigate();
   const [carts, setCarts] = useState<Cart[]>(initialCarts);
   const [openModal, setOpenModal] = useState(false);
+  const [openEditModal, setOpenEditModal] = useState(false);
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
+  const [selectedCart, setSelectedCart] = useState<Cart | null>(null); // Carrinho selecionado para edição
+  const [editedCodigo, setEditedCodigo] = useState<string>(''); // Estado para o código editado
 
   const handleOpen = () => setOpenModal(true);
   const handleClose = () => setOpenModal(false);
+  const handleOpenEditModal = (cart: Cart) => {
+    setSelectedCart(cart);
+    setEditedCodigo(cart.codigo); // Preencher o campo de edição com o código do carrinho
+    setOpenEditModal(true);
+  };
+  const handleCloseEditModal = () => setOpenEditModal(false);
 
   const handleAddCart = (codigo: string) => {
     const newCart: Cart = { id: carts.length + 1, codigo, ativo: true };
@@ -43,6 +54,14 @@ const CartList: React.FC = () => {
     setCarts(carts.filter(cart => cart.id !== id));
   };
 
+  const handleEditCart = () => {
+    if (selectedCart) {
+      setCarts(carts.map(cart => cart.id === selectedCart.id ? { ...cart, codigo: editedCodigo } : cart));
+      handleCloseEditModal();
+      setSuccess(true);
+    }
+  };
+
   const handleCloseSnackbar = () => {
     setError(false);
     setSuccess(false);
@@ -54,7 +73,6 @@ const CartList: React.FC = () => {
 
   return (
     <Box sx={{ padding: 2, boxShadow: '0 8px 16px rgba(0, 0, 0, 0.5);', minWidth: '100vh' }}>
-
       <Box className="box" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', mt: 2 }}>
         <IconButton onClick={handleRedirect}>
           <KeyboardReturn sx={{ color: 'white' }} />
@@ -71,7 +89,8 @@ const CartList: React.FC = () => {
               <TableCell sx={{ color: 'var(--cor-texto)' }}>ID</TableCell>
               <TableCell sx={{ color: 'var(--cor-texto)' }}>Código</TableCell>
               <TableCell sx={{ color: 'var(--cor-texto)' }}>Ativo</TableCell>
-              <TableCell sx={{ color: 'var(--cor-texto)' }}>#</TableCell>
+              <TableCell sx={{ color: 'var(--cor-texto)' }}>Status</TableCell>
+              <TableCell sx={{ color: 'var(--cor-texto)' }}>Ações</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -101,6 +120,11 @@ const CartList: React.FC = () => {
                   />
                 </TableCell>
                 <TableCell>
+                  <IconButton color="primary" onClick={() => handleOpenEditModal(cart)}>
+                    <BatteryChargingFullIcon />
+                  </IconButton>
+                </TableCell>
+                <TableCell>
                   <IconButton color="error" onClick={() => handleDelete(cart.id)}>
                     <DeleteIcon sx={{ color: '#bf2121' }} />
                   </IconButton>
@@ -119,9 +143,18 @@ const CartList: React.FC = () => {
       <Box className="box" sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2 }}>
         <Button variant="outlined" sx={{ ml: 1, color: 'var(--cor-texto)', borderColor: 'var(--cor-botao)' }} onClick={handleOpen}>Adicionar Cart</Button>
       </Box>
+
+      <StatusCartModal
+        open={openEditModal}
+        onClose={handleCloseEditModal}
+        cart={selectedCart}
+        onEdit={handleEditCart}
+      />
+
+
       <Snackbar open={error || success} autoHideDuration={6000} onClose={handleCloseSnackbar}>
         <Alert onClose={handleCloseSnackbar} severity={error ? 'error' : 'success'}>
-          {error ? 'Por favor, insira um código válido!' : 'Cart adicionado com sucesso!'}
+          {error ? 'Por favor, insira um código válido!' : 'Cart atualizado com sucesso!'}
         </Alert>
       </Snackbar>
     </Box >
---
+++ src/components/Cart/StatusCartModal/StatusCartModal.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Box, Modal, Typography } from '@mui/material';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+
+// Tipos
+interface Cart {
+    id: number;
+    codigo: string;
+    ativo: boolean;
+}
+
+interface StatusCartModalProps {
+    open: boolean;
+    onClose: () => void;
+    cart: Cart | null;
+    onEdit: (codigo: string) => void;
+}
+
+const distanceData = [
+    { name: 'Dia 1', distance: 2 },
+    { name: 'Dia 2', distance: 3 },
+    { name: 'Dia 3', distance: 5 },
+    { name: 'Dia 4', distance: 4 },
+    { name: 'Dia 5', distance: 7 }
+];
+
+const batteryLevel = 65;
+const batteryData = [
+    { name: 'Nível Atual', value: batteryLevel },
+    { name: 'Restante', value: 100 - batteryLevel },
+];
+
+const COLORS = ['#0088FE', '#00C49F'];
+
+const StatusCartModal: React.FC<StatusCartModalProps> = ({ open, onClose, cart }) => {
+    return (
+        <Modal
+            open={open}
+            onClose={onClose}
+            aria-labelledby="edit-cart-modal"
+            aria-describedby="edit-cart-modal-description"
+        >
+            <Box sx={{ boxShadow: '0 8px 16px rgba(255, 255, 255, 0.5);', padding: 4, borderRadius: 1, width: 400, margin: '100px auto', backgroundColor: 'var(--cor-fundo)' }}>
+                <Typography id="edit-cart-modal" variant="h6" component="h2">
+                    Status do  Carrinho
+                </Typography>
+                <Typography
+                    sx={{
+                        border: '1px solid #ccc',
+                        borderRadius: '4px',
+                        padding: '16px',
+                        margin: '16px 0',
+                        overflowX: 'auto',
+                    }} variant="body1" component="pre">
+                    ID do Carrinho: {cart?.id}
+                </Typography>
+                <Typography variant="h6" component="h2" sx={{ mt: 3 }}>
+                    Distância Percorrida (km)
+                </Typography>
+                <LineChart width={300} height={200} data={distanceData} margin={{ top: 20, right: 30, left: 20, bottom: 0 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Line type="monotone" dataKey="distance" stroke="#8884d8" activeDot={{ r: 8 }} />
+                </LineChart>
+
+                <Typography variant="h6" component="h2" sx={{ mt: 3 }}>
+                    Nível de Bateria (%)
+                </Typography>
+                <ResponsiveContainer width="100%" height={200}>
+                    <PieChart>
+                        <Pie
+                            data={batteryData}
+                            cx="50%"
+                            cy="50%"
+                            innerRadius={60}
+                            outerRadius={80}
+                            fill="#8884d8"
+                            paddingAngle={5}
+                            dataKey="value"
+                        >
+                            {batteryData.map((entry, index) => (
+                                <Cell key={`${entry.name}-cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                        <text x="50%" y="50%" textAnchor="middle" dominantBaseline="middle" fill="white" fontSize={24}>
+                            {batteryLevel}%
+                        </text>
+                    </PieChart>
+                </ResponsiveContainer>
+
+            </Box>
+        </Modal>
+    );
+};
+
+export default StatusCartModal;
